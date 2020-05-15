1df58e1506554b39517914c31d35e29f433f4a32
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-15 19:21:20 -0300

    Cabecalho

--- src/pontuacao.java
+++ src/pontuacao.java
@@ -6,9 +6,15 @@ public class pontuacao {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
 		
-		
-		
+		// Cabeçalho
+
+				System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+				System.out.println("--------------------------------");
+				System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		
 
 	}
