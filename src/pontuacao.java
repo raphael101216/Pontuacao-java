d3314d6da67a112b0069511859d7d9072329c2f2
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-15 19:30:43 -0300

    Leitura do volume

--- src/pontuacao.java
+++ src/pontuacao.java
@@ -10,13 +10,22 @@ public class pontuacao {
 		Scanner sc = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.00");
 		
+		int quantCompra, inadimplencia, formaPagamento, ScorevolumeCompras, scoreInadimplencia, scoreFormaPagamento, classificacaoFinal;
+		double tiketMedio;
+		
 		// Cabeçalho
 
 				System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 				System.out.println("--------------------------------");
 				System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		
-
+		//Leitura do volume
+				
+				System.out.println();
+				System.out.print("Quantas compras o cliente fez no último ano?");
+				quantCompra = sc.nextInt();
+				System.out.print("Qual o ticket médio?");
+				tiketMedio = sc.nextDouble();
 	}
 
 }
